ba1b9bf1ab2a9e3a162663cea3a0665dcb42c356
woongsnote <woongsnote@gmail.com>
2024-04-09 13:56:02 +0900

    Add PostSkeleton component for loading state and update Post component styling

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -1,7 +1,7 @@
 export const Post = ({ content }: { content: string }) => {
   return (
     <div
-      className="mt-4 w-full prose p-2 lg:max-w-3xl mx-auto break-keep overflow-scroll"
+      className="mt-4 w-full prose p-2 lg:max-w-3xl mx-auto break-keep"
       dangerouslySetInnerHTML={{ __html: content }}
     />
   );
---
+++ app/posts/[slug]/loading.tsx
@@ -0,0 +1,5 @@
+import { PostSkeleton } from "@/app/components/PostSkeleton";
+
+export default function Loading() {
+  return <PostSkeleton />;
+}
