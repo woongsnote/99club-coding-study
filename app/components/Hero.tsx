6e54de88bfc7512c096a831085cb9562d02f08c1
woongsnote <woongsnote@gmail.com>
2024-04-22 08:18:52 +0900

    style: UI update

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,7 +2,7 @@ import { SocialLinks } from "@/app/components/SocialLinks";
 
 export const Hero = () => {
   return (
-    <section className="w-full py-8 flex flex-col items-center gap-2 space-y-4 text-center break-keep">
+    <section className="w-full py-8 flex flex-col items-center gap-2 space-y-4 text-center break-keep lg:py-12">
       <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
         99클럽 코테 스터디
       </h1>
